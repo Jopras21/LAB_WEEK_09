d53eb84da2d9b4bf1e60d26f3b8fa2d00fe33ad8
Jopras21 <sun.phin098@gmail.com>
2025-11-09 13:05:29 +0700

    Commit No. 1 – Building Jetpack Compose UI

---
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.example.lab_week_09
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.Home
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+
+// Previously we extend AppCompatActivity
+// now we extend ComponentActivity
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Here, we use setContent instead of setContentView
+        setContent {
+            // Here, we wrap our content with the theme
+            // You can check out LAB_WEEK_09Theme inside Theme.kt
+            LAB_WEEK_09Theme {
+                // A surface container using the 'background' color from the theme
+                Surface (
+                    // We use Modifier.fillMaxSize() to make the surface fill the whole screen
+                    modifier = Modifier.fillMaxSize(),
+                    // We use MaterialTheme.colorScheme.background to get the background color and set it as the color or the surface
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val list = listOf("Tanu", "Tina", "Tono")
+                    // Here, we call the Home composable
+                    Home(list)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Home(
+    items: List<String>,
+) {
+    LazyColumn {
+        item {
+            Column (
+                modifier = Modifier.padding(16.dp).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // Here, we use Text to display a text
+                Text(
+                    text = stringResource(
+                        id = R.string.enter_item
+                    )
+                )
+                TextField (
+                    value = "",
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number
+                    ),
+
+                    onValueChange = {
+                    }
+                )
+
+                Button(onClick = {}){
+                    Text(text = stringResource(
+                        id = R.string.button_click)
+                    )
+                }
+            }
+        }
+        items(items) { item ->
+            Column (
+                modifier = Modifier.padding(vertical = 4.dp).fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = item)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewHome() {
+    Home(listOf("Tanu", "Tina", "Tono"))
+}
